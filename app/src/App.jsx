d72cc2de6eea2743a8d8108ac252a724464139b8
Omer Habib <omer.habib62@gmail.com>
2023-10-13 09:41:51 +0500

    1.5

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -15,11 +15,12 @@ const Part = ({ part, exercise}) =>
   )  
 }
 
-const Content = ( {content} ) => {
+const Content = ( props ) => {
+  
   return (    
     <div>
       {
-        content.map((element, index) => (
+        props.parts.map((element, index) => (
           <Part key={index} part={element.name} exercise={element.exercises} />
         ))
       }
@@ -47,30 +48,31 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
-  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
   const totalData = [
-    parts[0].exercises, parts[1].exercises, parts[2].exercises
+    course.parts[0].exercises, course.parts[1].exercises, course.parts[2].exercises
   ];
-
   return (
+    
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total parts={totalData} />
     </div>
   );
